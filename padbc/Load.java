5000c29b75a8bfcac5b8ae013e6848822b00fe99
Chris Mosier <chris@powerdata.com>
2013-05-16 20:59:50 -0700

    add loads and generators to padbc

---
+++ padbc/Load.java
@@ -0,0 +1,24 @@
+package com.powerdata.openpa.padbc;
+
+public class Load extends BaseObject
+{
+	private LoadList _list;
+	
+	public Load(int ndx, LoadList list)
+	{
+		super(ndx);
+		_list = list;
+	}
+	
+	@Override
+	public String getID() {return _list.getID(getIndex());}
+	public float getActvPwr() {return _list.getActvPwr(getIndex());}
+	public float getReacPwr() {return _list.getReacPwr(getIndex());}
+
+	/* methods to use if we want external-world units */
+//	public float getMW() {return _list.getMW(getIndex());}
+//	public float getMVAr() {return _list.getMVAr(getIndex());}
+
+	public void updateActvPwr(float p) {_list.updateActvPwr(getIndex(), p);}
+	public void updateReacPwr(float q) {_list.updateReacPwr(getIndex(), q);}
+}
