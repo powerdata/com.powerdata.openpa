536f3426d4a9b097a2308ed53fda31a09088cea4
Chris Mosier <chris@powerdata.com>
2013-06-16 22:02:31 -0700

    add branch object out

--- psse/TransformerOut.java
+++ psse/TransformerOut.java
@@ -18,4 +18,23 @@ public class TransformerOut extends BaseObject
 		return null;
 	}
 
+	/** set from-side active power in p.u. */
+	public void setFromActvPwr(float p) {/*TODO: */}
+	/** set to-side active power in p.u. */
+	public void setToActvPwr(float p) {/*TODO: */}
+	/** set from-side reactive power in p.u. */
+	public void setFromReacPwr(float q) {/*TODO: */}
+	/** set to-side reactive power in p.u. */
+	public void setToReacPwr(float q) {/*TODO: */}
+	
+	/** set from-side MW */
+	public void setFromMW(float mw) {/*TODO: */}
+	/** set to-side MW */
+	public void setToMW(float mw) {/*TODO: */}
+	/** set from-side MVAr */
+	public void setFromMVAr(float mvar) {/*TODO: */}
+	/** set to-side MVAr */
+	public void setToMVAr(float mvar) {/*TODO: */}
+
+	
 }
