ed2a09960186dc33264b91bb0c0c479eb9955fc2
Chris Mosier <chris@powerdata.com>
2014-08-05 12:59:46 -0700

    remove Unknown enum from ListMetaType

--- ListMetaType.java
+++ ListMetaType.java
@@ -4,5 +4,5 @@ public enum ListMetaType
 {
 	AnonymousGroup, Area, Owner, Station, VoltageLevel, Island, Bus, Switch, Line, 
 	SeriesCap, SeriesReac, Transformer, PhaseShifter, TwoTermDCLine, 
-	Gen, Load, ShuntReac, ShuntCap, SVC, SwitchedShunt, Unknown
+	Gen, Load, ShuntReac, ShuntCap, SVC, SwitchedShunt
 }
