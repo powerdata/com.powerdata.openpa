65a13581904027b21b1fb4f8e7ad74e933292ad7
Derek Robinson <derek@powerdata.com>
2015-06-09 10:22:26 -0700

    Added high and low step to RatioTapChanger

--- tools/psmfmt/RatioTapChanger.java
+++ tools/psmfmt/RatioTapChanger.java
@@ -2,7 +2,7 @@ package com.powerdata.openpa.tools.psmfmt;
 
 public enum RatioTapChanger implements VersionedDoc
 {
-	ID, Name, TransformerWinding, TapNode, MinTap, MaxTap, NeutralTap, StepSize, MinKV, MaxKV, NeutralKV;
+	ID, Name, TransformerWinding, TapNode, MinTap, MaxTap, NeutralTap, LowStep, HighStep, StepSize, MinKV, MaxKV, NeutralKV;
 	
 	@Override
 	public String getVersion() {return "1.10";}
