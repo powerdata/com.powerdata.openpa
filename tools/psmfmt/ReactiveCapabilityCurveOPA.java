97d643ae229b4cc6bcec628ea8d43d6a69fc6b4d
Derek Robinson <derek@powerdata.com>
2015-06-11 09:41:31 -0700

    Added support for a second FmtInfo so that both crv1 and crv2 could be exported properly

--- tools/psmfmt/ReactiveCapabilityCurveOPA.java
+++ tools/psmfmt/ReactiveCapabilityCurveOPA.java
@@ -1,37 +1,69 @@
 package com.powerdata.openpa.tools.psmfmt;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 import com.powerdata.openpa.GenList;
 import com.powerdata.openpa.PAModel;
 import com.powerdata.openpa.PAModelException;
 
 public class ReactiveCapabilityCurveOPA extends ExportOpenPA<GenList>
 {
-
+	protected FmtInfo[] _fi;
+	
 	public ReactiveCapabilityCurveOPA(PAModel m) throws PAModelException
 	{
 		super(m.getGenerators(), ReactiveCapabilityCurve.values().length);
+		_fi = new FmtInfo[ReactiveCapabilityCurve.values().length];
 		assignCurve1();
 		assignCurve2();
 	}
 
 	void assignCurve1() throws PAModelException
 	{
-		assign(ReactiveCapabilityCurve.ID, i -> String.format("\"%s_crv1\"",_list.getID(i)));
+		assign(ReactiveCapabilityCurve.ID,
+				i -> String.format("\"%s_crv1\"",_list.getID(i)));
 		assign(ReactiveCapabilityCurve.SynchronousMachine,
-			i -> String.format("\"%s\"", _list.get(i)));
-		assign(ReactiveCapabilityCurve.MW, i -> String.valueOf(_list.getOpMinP(i)));
-		assign(ReactiveCapabilityCurve.MinMVAr, i -> String.valueOf(_list.getMinQ(i)));
-		assign(ReactiveCapabilityCurve.MaxMVAr, i -> String.valueOf(_list.getMaxQ(i)));
+				i -> String.format("\"%s\"", _list.get(i)));
+		assign(ReactiveCapabilityCurve.MW, 
+				i -> String.valueOf(_list.getOpMinP(i)));
+		assign(ReactiveCapabilityCurve.MinMVAr, 
+				i -> String.valueOf(_list.getMinQ(i)));
+		assign(ReactiveCapabilityCurve.MaxMVAr,
+				i -> String.valueOf(_list.getMaxQ(i)));
 	}
 	
 	void assignCurve2() throws PAModelException
 	{
-		assign(ReactiveCapabilityCurve.ID, i -> String.format("\"%s_crv2\"",_list.getID(i)));
+		assign(ReactiveCapabilityCurve.ID,
+				i -> String.format("\"%s_crv2\"",_list.getID(i)),
+				_fi);
 		assign(ReactiveCapabilityCurve.SynchronousMachine,
-			i -> String.format("\"%s\"", _list.get(i)));
-		assign(ReactiveCapabilityCurve.MW, i -> String.valueOf(_list.getOpMaxP(i)));
-		assign(ReactiveCapabilityCurve.MinMVAr, i -> String.valueOf(_list.getMinQ(i)));
-		assign(ReactiveCapabilityCurve.MaxMVAr, i -> String.valueOf(_list.getMaxQ(i)));
+				i -> String.format("\"%s\"", _list.get(i)),
+				_fi);
+		assign(ReactiveCapabilityCurve.MW, 
+				i -> String.valueOf(_list.getOpMaxP(i)),
+				_fi);
+		assign(ReactiveCapabilityCurve.MinMVAr,
+				i -> String.valueOf(_list.getMinQ(i)),
+				_fi);
+		assign(ReactiveCapabilityCurve.MaxMVAr,
+				i -> String.valueOf(_list.getMaxQ(i)),
+				_fi);
+	}
+	
+	@Override
+	public void export(File outputdir) throws IOException
+	{
+		PrintWriter pw = new PrintWriter(new BufferedWriter(
+				new FileWriter(new File(outputdir, getPsmFmtName()+".csv"))));
+		printHeader(pw);
+		printData(pw, _finfo, getCount());
+		printData(pw, _fi, getCount());
+		pw.close();
 	}
 	
 	@Override
