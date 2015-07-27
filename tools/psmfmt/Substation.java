f8964a5951d507ec43d43b5c4517bccbcb8fad92
Derek Robinson <derek.dt@gmail.com>
2015-07-27 11:08:51 -0700

    Added LoadArea

--- tools/psmfmt/Substation.java
+++ tools/psmfmt/Substation.java
@@ -2,7 +2,7 @@ package com.powerdata.openpa.tools.psmfmt;
 
 public enum Substation implements VersionedDoc
 {
-	ID, Name, Organization, ControlArea;
+	ID, Name, Organization, ControlArea, LoadArea;
 
 	@Override
 	public String getVersion() {return "1.10";}
